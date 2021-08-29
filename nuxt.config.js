100f460f72696da15224f149d3890cff55ea384c
Dan Cruzat <thecruzat@gmail.com>
2021-08-29 13:41:39 -0700

    oops meta is good

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Four Snapshots in the Life of an Oddball' },
     ],
     link: [
       // { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Nunito:wght@200;400&family=Rubik&display=swap' },
